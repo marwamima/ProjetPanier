bae8f75e655e9a98f4e9ba7b00f653365b4428cb
marwamima <marwahermi19@gmail.com>
2022-05-07 23:46:01 +0200

    projet de panier 2

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ var total=0;
       for (i=0;i<numberproduit;i++){
           a[i]=document.querySelectorAll('.price')[i]; 
           b[i]=document.querySelectorAll('.qty')[i].value;
-          c[i]=a[i]*b[i];
-          return total+=c[i];
+          c[i]=a[i]*b[i];  //la somme de  chaque produit :quantité*prix
+          document.querySelectorAll('.sum')[i]=c[i];
+          return total+=c[i]; // la somme total de tout les produits 
 
         
        } 
